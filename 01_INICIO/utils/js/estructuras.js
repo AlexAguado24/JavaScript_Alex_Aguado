4f89669ce5b4a6265c0080af258c5da901813369
AlexAguado24 <aaguado131@gmail.com>
2022-06-07 09:47:22 +0200

    update

---
+++ 01_INICIO/utils/js/estructuras.js
@@ -0,0 +1,52 @@
+// if if else switch while do while for foreach (especial) for of for on break go to
+
+// if if else 
+//let acierto = true;
+/* if (acierto){
+    console.log("es true");
+} else {
+    console.log("es false");
+} */
+
+
+//let nota = 5;
+/* if (nota >= 5) {
+    console.log("examen aprobado");
+    
+} else {
+    console.log("examen suspenso");
+    
+} */
+
+// if ternario
+
+// condicion ? true : false
+
+//acierto ? console.log('examen aprobado') : console.log("examen suspenso");
+
+// if 
+/* acierto & console.log("examen aprobado");
+
+let nota = 6;
+
+switch (nota) {
+    case 1:
+        console.log("dato 1");
+        break;
+    case 2:
+        console.log("dato 2");
+        break;
+    case 3:
+        console.log("dato 3");
+        break;
+    default:
+        console.log("dato incorrecto");
+        break;
+} */
+
+let numero1 = prompt(`Por favor introduce el primer numero`,"numero");
+console.log(numero1);
+
+let numero2 = prompt(`Por favor introduce el segundo numero`,"numero");
+console.log(numero2);
+
