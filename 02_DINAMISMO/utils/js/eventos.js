7b5cd9313a3d9eb59cbe3c9a503a0023b6457733
AlexAguado24 <aaguado131@gmail.com>
2022-06-10 11:25:13 +0200

    .

--- 02_DINAMISMO/utils/js/eventos.js
+++ 02_DINAMISMO/utils/js/eventos.js
@@ -1,11 +1,20 @@
 let boton_agregar = document.querySelector("#boton_agregar");
 let input_agregar = document.querySelector("#texto_li");
-boton_agregar.addEventListener("click", element => {
+let contar_pulsaciones = document.querySelector("span")
+let contador = 0;
+/* boton_agregar.addEventListener("click", element => {
   console.log(element);
-});
+}); */
 
-input_agregar.addEventListener("keydown", () => {
+input_agregar.addEventListener("keyup", (event) => {
   console.log(input_agregar.value);
-});
+  contador++;
+    contar_pulsaciones.innerText = contador;
+}); 
+
+
+
+
+
 //declarar la funcion fuera (va a ser reutilizada?)
 //o dentro(solo ejecutada por el evento) --> anonima
