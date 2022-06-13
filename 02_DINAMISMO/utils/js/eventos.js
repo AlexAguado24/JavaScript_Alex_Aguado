9091b19a445d7138587a9411b3b43761556857f3
AlexAguado24 <aaguado131@gmail.com>
2022-06-13 09:53:18 +0200

    .

--- 02_DINAMISMO/utils/js/eventos.js
+++ 02_DINAMISMO/utils/js/eventos.js
@@ -1,35 +1,37 @@
 let boton_agregar = document.querySelector("#boton_agregar");
 let input_agregar = document.querySelector("#texto_li");
-let contar_pulsaciones = document.querySelector("span");
 let contador = 0;
-boton_agregar.addEventListener("click", element => {
-  //console.log(element);
-  //let contenido = input_agregar.value;
-  //console.log(contenido);
+let spanPulsaciones = document.querySelector("span");
+
+boton_agregar.addEventListener("click", (element) => {
+  let contenido = input_agregar.value;
+  // console.log(contenido);
   // agregar nodos
   /*let lista = document.querySelector("#lista_primeros");
-  let nodoLi = document.createElement("li");
-  nodoLi.innerText = contenido;
-  //nodoLi.classList.add("list-group-item");
-  nodoLi.setAttribute('id','valor')
-  lista.appendChild(nodoLi);*/
-  // modificar html
-  let contenido = input_agregar.value;
-  if (contenido == undefined) {
-    console.log("Por favor, añade el nuevo contenido");
-    
-  } else {
+  let nodoLI = document.createElement("li");
+  nodoLI.innerText = contenido;
+  //nodoLI.classList.add("list-group-item");
+  nodoLI.setAttribute("class", "list-group-item");
+  nodoLI.setAttribute("id", "valor");
+  lista.appendChild(nodoLI);*/
+  // modificar el html
+  if (contenido != "") {
     let lista = document.querySelector("#lista_primeros");
-    lista.innerHTML = `${lista.innerHTML} <li class="list-group-item">${contenido}</li>`;
+    lista.innerHTML = `${lista.innerHTML} <li class='list-group-item'>${contenido}</li>`;
+  } else {
+    console.log("contenido vacio, introduce nueva asignatura");
   }
-
 });
 
-/*input_agregar.addEventListener("keyup", event => {
-  console.log(input_agregar.value);
+input_agregar.addEventListener("keydown", (event) => {
+  // sacar la tecla pulsada por consola
+  // console.log(event.key);
+  // console.log(input_agregar.value);
   contador++;
-  contar_pulsaciones.innerText = contador;
-});*/
+  spanPulsaciones.innerText = contador;
+});
 
-//declarar la funcion fuera (va a ser reutilizada?)
-//o dentro(solo ejecutada por el evento) --> anonima
+/*  el en parrafo que está configurado al final en el 0
+ deberá aparecer el número de pulsaciones realizadas (dinámico)
+ declarar la funcion fuera (va a ser reutilizada?)
+ o dentro(solo ejecutada por el evento) --> anónima */
